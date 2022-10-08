e2f67dc3d2860328a7045b38f37ebaaad57d78b5
妖雨纯 <876451336@qq.com>
2022-10-08 12:40:16 +0800

    ✨ feat(redis): 完善redis订阅

--- src/core/redis.service.ts
+++ src/core/redis.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { Injectable, Logger } from '@nestjs/common'
 import { ConfigService } from '@nestjs/config'
 import { InjectRedis, Redis } from '@nestjs-modules/ioredis'
 import { Observer } from './core.observer'
@@ -6,19 +6,19 @@ import * as IoRedis from 'ioredis'
 
 @Injectable()
 export class RedisService {
-	private current: string | undefined = undefined
+	private done: unknown | undefined
 	private readonly observer: Observer<Record<string, unknown>> = new Observer()
-	constructor(private readonly config: ConfigService, @InjectRedis() private readonly client: Redis) {}
-
-	private isCurrent(value: string): boolean {
-		return this.current === value
+	private readonly trigger: Observer<Record<string, unknown>> = new Observer()
+	constructor(private readonly config: ConfigService, @InjectRedis() private readonly client: Redis) {
+		this.initSubscribe().then(r => Logger.log(r))
 	}
 
-	public subscribe(inc: string | ((x: string) => boolean)): Promise<Observer<Record<string, unknown>>> {
-		return new Promise((resolve, reject) => {
+	/**初始化订阅消息**/
+	private initSubscribe() {
+		return new Promise(resilve => {
 			try {
-				const { client, config, observer } = this
-				const command = `__keyevent@${this.config.get('REDIS_DB')}__:expired`
+				const { client, config, trigger } = this
+				const command = `__keyevent@${config.get('REDIS_DB')}__:expired`
 				client.send_command('config', ['set', 'notify-keyspace-events', 'Ex'], () => {
 					const instance = new IoRedis({
 						host: config.get('REDIS_HOST'),
@@ -28,20 +28,39 @@ export class RedisService {
 						keyPrefix: config.get('REDIS_KEYPREFIX')
 					})
 					instance.subscribe(command, () => {
-						instance.on('message', (cmd, key) => {
-							if (this.isCurrent(key) && typeof inc === 'string' && key.startsWith(inc)) {
-								observer.emit('message', { cmd, key })
-							} else if (this.isCurrent(key) && typeof inc === 'function' && inc(key)) {
-								observer.emit('message', { cmd, key })
-							}
-							this.current = key
-						})
+						instance.on('message', (cmd, key) => trigger.emit('message', { cmd, key }))
 					})
+					resilve('Redis: 订阅成功')
 				})
-				resolve(observer)
 			} catch (e) {
-				reject(e)
+				Logger.error('订阅失败: ', e)
 			}
+		})
+	}
+
+	private isCommand(command: string | ((x: string) => boolean), key: string) {
+		return (
+			(typeof command === 'string' && key.startsWith(command)) || (typeof command === 'function' && command(key))
+		)
+	}
+
+	public subscribe(
+		command: string | ((x: string) => boolean),
+		handler?: (e: { cmd: string; key: string }) => void
+	): Promise<Observer<Record<string, unknown>>> {
+		return new Promise(resolve => {
+			const { trigger, observer } = this
+			const onTrigger = (e: { cmd: string; key: string }) => {
+				if (this.isCommand(command, e.key)) {
+					handler?.(e)
+					observer.emit('message', e)
+				}
+			}
+			if (!this.done) {
+				/**订阅开启**/
+				this.done = trigger.on('message', onTrigger)
+			}
+			resolve(observer)
 		})
 	}
 
