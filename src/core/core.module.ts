26d6060c5c2f5b34bc929b6c5fe77ebf0181a9fd
妖雨纯 <876451336@qq.com>
2022-10-09 15:09:04 +0800

    ✨ feat(dispatch): 新增调度模块

--- src/core/core.module.ts
+++ src/core/core.module.ts
@@ -1,5 +1,4 @@
 import { Module, Global } from '@nestjs/common'
-import { ScheduleModule } from '@nestjs/schedule'
 //service
 import { CoreService } from './core.service'
 import { EntityService } from './entity.service'
@@ -12,6 +11,7 @@ import { BezierEntity } from '@/entity/bezier.entity'
 import { FileEntity } from '@/entity/file.entity'
 //module
 import { QueueModule } from '@/module/queue/queue.module'
+import { DispatchModule } from '@/module/dispatch/dispatch.module'
 import { UploadModule } from '@/module/upload/upload.module'
 import { FlowChartModule } from '@/module/flow-chart/flow-chart.module'
 import { WeChatModule } from '@/module/we-chat/we-chat.module'
@@ -19,9 +19,9 @@ import { WeChatModule } from '@/module/we-chat/we-chat.module'
 @Global()
 @Module({
 	imports: [
-		ScheduleModule.forRoot(),
 		TypeOrmModule.forFeature([ChartEntity, ChunkEntity, BezierEntity, FileEntity]),
 		QueueModule,
+		DispatchModule,
 		UploadModule,
 		FlowChartModule,
 		WeChatModule
