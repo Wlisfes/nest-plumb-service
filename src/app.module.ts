bde0c0c4d295f642182e39ba7c370734d62ce00d
妖雨纯 <876451336@qq.com>
2022-10-09 11:11:29 +0800

    ✨ feat(schedule): 新增定时任务包

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common'
 import { ConfigModule, ConfigService } from '@nestjs/config'
+import { ScheduleModule } from '@nestjs/schedule'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { RedisModule } from '@nestjs-modules/ioredis'
 import { APP_INTERCEPTOR, APP_FILTER } from '@nestjs/core'
@@ -12,6 +13,7 @@ import { CoreModule } from '@/core/core.module'
 @Module({
 	imports: [
 		ConfigModule.forRoot({ isGlobal: true }),
+		ScheduleModule.forRoot(),
 		RedisModule.forRootAsync({
 			inject: [ConfigService],
 			useFactory: (config: ConfigService) => {
